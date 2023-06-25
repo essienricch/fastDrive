c5d75c8f64af332a25dfeff5530c2f36a1cc6409
essienricch <essienrichard98@gmail.com>
2023-06-25 15:14:24 -0700

    include env variable

--- src/main/java/project/colon/fastdrive/FastDriveApplication.java
+++ src/main/java/project/colon/fastdrive/FastDriveApplication.java
@@ -4,14 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 @Slf4j
 public class FastDriveApplication {
 
     public static void main(String[] args) {
-//        String port = System.getenv("${PORT}");
+
+        Properties properties = new Properties();
+        try{
+            FileInputStream file = new FileInputStream("C:\\Users\\New Owner\\IdeaProjects\\fastDrive\\src\\main\\resources\\application.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String port = properties.getProperty("server.port");
         SpringApplication.run(FastDriveApplication.class, args);
-//        log.info("Server is starting @ port {} >>>>>> ", port);
+        log.info("Server is starting @ port {} >>>>",port);
     }
 
 }
--- src/test/java/project/colon/fastdrive/FastDriveApplicationTest.java
+++ src/test/java/project/colon/fastdrive/FastDriveApplicationTest.java
@@ -8,8 +8,12 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,18 +26,33 @@ class FastDriveApplicationTest {
     void contextLoads(){
     }
 
-//    @Test
-//    void dataConnectionTest(){
-//
-//        DataSource dataSource = new DriverManagerDataSource("jdbc:postgresql://localhost:5432/fastdrive_db");
-//
-//        try {
-//            Connection connection = dataSource.getConnection("${USERNAME}", "${PASSWORD}");
-//            assertThat(connection).isNotNull();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    @Test
+    void dataConnectionTest(){
+
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream file = new FileInputStream("C:\\Users\\New Owner\\IdeaProjects\\fastDrive\\src\\main\\resources\\secrets.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String username = properties.getProperty("spring.datasource.username");
+        log.info(username);
+        String password = properties.getProperty("spring.datasource.password");
+        log.info(password);
+
+        DataSource dataSource = new DriverManagerDataSource("jdbc:postgresql://localhost:5432/fastdrive_db");
+
+        try {
+            Connection connection = dataSource.getConnection(username, password);
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
